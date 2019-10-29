3fa37b0f0f85a49a56dac19c3fac3324a27481e7
Michaela Jackson <mjack132@students.kennesaw.edu>
2019-10-29 19:30:54 -0400

    Work on PacmanDriver, still figuring things out. Coming up with errors that the files cannot be found, but working on it.

--- Pacman/PacmanDriver.cs
+++ Pacman/PacmanDriver.cs
@@ -10,6 +10,35 @@ namespace Pacman
     class PacmanDriver
     {
 
+        public Pacman() //default constructor, starts pacman off in the center of the screen
+        {
+            int x = 5;
+            int y = 5;
+        }
+
+        public void locator()
+        {
+            Console.WriteLine("Current location -\tX: " + x + "\tY: " + y);
+            Console.WriteLine("(U)p, (D)own, (L)eft, (R)ight, or (Q)uit: ");
+            char answer = Console.ReadLine();
+            
+            switch (answer)
+            {
+                case 'U':
+                    pac.Up();
+                    break;
+                case 'D':
+                    pac.Down();
+                    break;
+                case 'L':
+                    pac.Left();
+                    break;
+                case 'R':
+                    pac.Right();
+                    break;
+            }
+        }
+
         //Method that will create our object of pac-man and will handle input and output of our program.
         public static void Main(String[] args)
         {
@@ -19,6 +48,12 @@ namespace Pacman
             //1. create a pac-man object.
             //2. update its position based on the user input.
             //3. repeat step 2 until the player inputs 'Q'.
+
+            Pacman pac = new Pacman();
+            Console.WriteLine(pac);
+
         }
+
+
     }
 }
